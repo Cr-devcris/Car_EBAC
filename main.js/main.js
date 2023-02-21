e2c61a35158929e13fc23f81223e0b015d46b737
Cristiano Ribeiro <cristiano.ribeirosantos33@gmail.com>
2023-02-21 18:55:27 -0400

    add validador
    validando script

---
+++ main.js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+            $('#carousel-imagens').slick({autoplay: true});
+            $('.menu-hamburguer').click(function(){
+                $('nav').slideToggle();
+            })
+
+            $('#telefone').mask('(00) 00000-0000');
+
+            $('form').validate({
+                rules: {
+                    nome: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    telefone: {
+                        required: true
+                    },
+                    mensagem:{
+                        required: true,
+                    },
+                    veiculoDeInteresse:{
+                        required: true
+                    }
+                },
+                messages: {
+                    nome: 'Digite seu nome completo'
+                },
+                submitHandler: function(form) {
+                    console.log(form)
+                },
+                invalidHandler: function(evento, validador) {
+                    let camposIncorretos = validador.numberOfInvalids();
+                    if (camposIncorretos) {
+                        alert(`Existem ${camposIncorretos} campos incorretos`)
+                    }
+                }
+            })
+            $('.lista-veiculos button').click(function() {
+                const destino = $('#contato');
+                const nomeVeiculo = $(this).parent().find('h3').text();
+                
+                $('#veiculo-interesse').val(nomeVeiculo);
+
+                $('html').animate({
+                    scrollTop: destino.offset().top
+                }, 1000)
+            })
+        })
